b2569d48e5a5153575331c23bff745cabeaf6a9f
THDWarstrike <89410413+THDWarstrike@users.noreply.github.com>
2022-03-29 12:09:38 -0400

    Add files via upload

---
+++ main.py
@@ -0,0 +1,18 @@
+msi_rtx5000_price = 4199.35
+gigabyte_aero_price = 4295.54
+razer_blade15_price = 3696.99
+asus_zephyrus_m16_price = 1849.79
+asus_rog_strix_advantage_edition = 1699.99
+gigabyte_aero_amoled_price = 4299.99
+
+print(max(msi_rtx5000_price, gigabyte_aero_price,razer_blade15_price,asus_zephyrus_m16_price,asus_rog_strix_advantage_edition,gigabyte_aero_amoled_price))
+print(min(msi_rtx5000_price, gigabyte_aero_price,razer_blade15_price,asus_zephyrus_m16_price,asus_rog_strix_advantage_edition,gigabyte_aero_amoled_price))
+print(round(msi_rtx5000_price))
+print(round(gigabyte_aero_price))
+print(round(razer_blade15_price))
+print(round(asus_zephyrus_m16_price))
+print(round(asus_rog_strix_advantage_edition))
+print(round(gigabyte_aero_amoled_price))
+
+sum1 = sum([msi_rtx5000_price, gigabyte_aero_price, razer_blade15_price, asus_zephyrus_m16_price,asus_rog_strix_advantage_edition, gigabyte_aero_amoled_price])
+print(round(sum1/6))
